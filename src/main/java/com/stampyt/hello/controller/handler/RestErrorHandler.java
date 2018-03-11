92635e94bf9e01ad91750ab7f2f99faa3d6b7c8f
ldalzotto <loic.dalzotto@hotmail.fr>
2018-03-11 10:59:31 +0100

    Add error logging

--- src/main/java/com/stampyt/hello/controller/handler/RestErrorHandler.java
+++ src/main/java/com/stampyt/hello/controller/handler/RestErrorHandler.java
@@ -3,6 +3,8 @@ package com.stampyt.hello.controller.handler;
 import com.stampyt.hello.service.exceptions.CarNotFound;
 import com.stampyt.hello.service.exceptions.GarageMaxCapacityNotDefined;
 import com.stampyt.hello.service.exceptions.GarageNotFound;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -17,6 +19,8 @@ import java.util.List;
 @RestControllerAdvice
 public class RestErrorHandler {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(RestErrorHandler.class);
+
     private static final String INVALID_FORMAT = "INVALID_FORMAT";
     private static final String BAD_REQUEST = "BAD_REQUEST";
     private static final String GARAGE_NOT_FOUND = "GARAGE_NOT_FOUND";
@@ -25,24 +29,32 @@ public class RestErrorHandler {
 
     @ExceptionHandler(GarageNotFound.class)
     public ResponseEntity<ExceptionMessage> handleGarageNotFound(HttpServletRequest request, GarageNotFound exception) {
+        LOGGER.error(exception.getMessage(), exception);
+
         ExceptionMessage exceptionMessage = new ExceptionMessage(GARAGE_NOT_FOUND, exception.getMessage());
         return new ResponseEntity<>(exceptionMessage, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(CarNotFound.class)
     public ResponseEntity<ExceptionMessage> handleCarNotFound(HttpServletRequest request, CarNotFound exception) {
+        LOGGER.error(exception.getMessage(), exception);
+
         ExceptionMessage exceptionMessage = new ExceptionMessage(CAR_NOT_FOUND, exception.getMessage());
         return new ResponseEntity<>(exceptionMessage, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(GarageMaxCapacityNotDefined.class)
     public ResponseEntity<ExceptionMessage> hangleGarageMaxCapacityNotDefined(HttpServletRequest request, GarageMaxCapacityNotDefined exception) {
+        LOGGER.error(exception.getMessage(), exception);
+
         ExceptionMessage exceptionMessage = new ExceptionMessage(GARAGE_MAX_CAPACITY_UNDEFINED, exception.getMessage());
         return new ResponseEntity<>(exceptionMessage, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ExceptionMessage> handleInvalidArgument(HttpServletRequest request, MethodArgumentNotValidException exception) {
+        LOGGER.error(exception.getMessage(), exception);
+
         List<ObjectError> objectErrors = exception.getBindingResult().getAllErrors();
         StringBuilder errorMessage = new StringBuilder();
         for (ObjectError objectError :
@@ -60,6 +72,7 @@ public class RestErrorHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ExceptionMessage> handleAnyErrors(HttpServletRequest request, Exception exception) {
+        LOGGER.error(exception.getMessage(), exception);
         ExceptionMessage exceptionMessage = new ExceptionMessage(BAD_REQUEST, exception.getMessage());
         return new ResponseEntity<>(exceptionMessage, HttpStatus.BAD_REQUEST);
     }
